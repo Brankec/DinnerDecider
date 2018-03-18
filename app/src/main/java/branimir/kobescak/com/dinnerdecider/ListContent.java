6bb4df6052b78cb5c970d9287e81fe307f833a76
Branimir <branimir.kobescak@gmail.com>
2018-03-18 14:21:30 +0100

    Added working expandable list view

--- app/src/main/java/branimir/kobescak/com/dinnerdecider/ListContent.java
+++ app/src/main/java/branimir/kobescak/com/dinnerdecider/ListContent.java
@@ -4,16 +4,12 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Gejmer on 3/13/2018.
@@ -21,9 +17,6 @@ import java.util.Map;
 
 public class ListContent extends AppCompatActivity {
     DatabaseHelper myDB = new DatabaseHelper(this);
-
-    List<String> foods;
-    Map<String, List<String>> info;
 
     ListView listFood;
 
@@ -37,91 +30,33 @@ public class ListContent extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listview_layout);
 
-        //listFood = (ListView) findViewById(R.id.list);
-
-        //Populates list view with data from the database
-        //populateListView();
-
-        //listData.setAdapter(listAdapter);
-
         listView = (ExpandableListView)findViewById(R.id.list);
-        initData();
+        populateListView();
         listAdapter = new ExpandableListAdapterClass(this,listDataHeader,listHash);
         listView.setAdapter(listAdapter);
     }
 
-
-    //Populate list view function
-    /*private void populateListView() {
-        foods = new ArrayList<>();
-        info = new HashMap<>();
-        Cursor data = myDB.getDatalist();
-
-        while(data.moveToNext()) {
-            foods.add(data.getString(1));
-        }
-    }*/
-
     //Populate list view function
     private void populateListView() {
-        ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
+        listDataHeader = new ArrayList<>();
+        listHash = new HashMap<>();
+        int index = 0;
 
         Cursor data = myDB.getDatalist();
         ArrayList<String> listData = new ArrayList<>();
 
-        HashMap item = new HashMap<String,String>();
-
         while(data.moveToNext()) {
-            listData.add(data.getString(0));
+            listDataHeader.add(data.getString(1));
 
-            //item.put( "line1", data.getString(1)); //this is for a 2 line list view
-            //item.put( "line2", data.getString(0));
-            //list.add( item );
+            List<String> info = new ArrayList<>();
+            info.add(data.getString(0));
+            info.add(data.getString(2));
+            info.add(data.getString(3));
+            info.add(data.getString(4));
+
+            listHash.put(listDataHeader.get(index),info);
+            index++;
         }
-
-        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
-
-        listFood.setAdapter(adapter);
-    }
-
-
-
-
-
-
-    private void initData() {
-        listDataHeader = new ArrayList<>();
-        listHash = new HashMap<>();
-
-        listDataHeader.add("EDMTDev");
-        listDataHeader.add("Android");
-        listDataHeader.add("Xamarin");
-        listDataHeader.add("UWP");
-
-        List<String> edmtDev = new ArrayList<>();
-        edmtDev.add("This is Expandable ListView");
-
-        List<String> androidStudio = new ArrayList<>();
-        androidStudio.add("Expandable ListView");
-        androidStudio.add("Google Map");
-        androidStudio.add("Chat Application");
-        androidStudio.add("Firebase ");
-
-        List<String> xamarin = new ArrayList<>();
-        xamarin.add("Xamarin Expandable ListView");
-        xamarin.add("Xamarin Google Map");
-        xamarin.add("Xamarin Chat Application");
-        xamarin.add("Xamarin Firebase ");
-
-        List<String> uwp = new ArrayList<>();
-        uwp.add("UWP Expandable ListView");
-        uwp.add("UWP Google Map");
-        uwp.add("UWP Chat Application");
-        uwp.add("UWP Firebase ");
-
-        listHash.put(listDataHeader.get(0),edmtDev);
-        listHash.put(listDataHeader.get(1),androidStudio);
-        listHash.put(listDataHeader.get(2),xamarin);
-        listHash.put(listDataHeader.get(3),uwp);
+        index = 0;
     }
 }
